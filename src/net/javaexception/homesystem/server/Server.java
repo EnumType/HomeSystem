7eda401a3040a9c70f211f8abf5e2cca74d09e13
TheJavaException <Anfinn@DESKTOP-EEIRP8G.fritz.box>
2020-01-26 17:59:42 +0100

    Version 1.0.8

--- src/net/javaexception/homesystem/server/Server.java
+++ src/net/javaexception/homesystem/server/Server.java
@@ -26,7 +26,7 @@ public class Server {
 	private static HashMap<InetAddress, Socket> sockets;
 	
 	public static void start() throws IOException {
-		Data.version = "v1.0.7";
+		Data.version = "v1.0.8";
 		
 		Log.write(Methods.createPrefix() + "Starting Server version: " + Data.version + " Port: " + Data.serverPort, true);
 		System.out.println("");
