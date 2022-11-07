739fa209da2333786a8abd6647c70a96ddaf186c
EnumType <46223727+EnumType@users.noreply.github.com>
2022-11-07 11:52:16 +0100

    Beta 1.0.9-14

--- src/net/enumtype/homesystem/server/Command.java
+++ src/net/enumtype/homesystem/server/Command.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List; //Test
+import java.util.List;
 
 public class Command {
 	public static void check(String command, Session session) throws UnknownCommandException {
